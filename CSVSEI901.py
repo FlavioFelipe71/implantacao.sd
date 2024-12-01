e2c6b801c65ad2d46576ad8a05d4f123cf729813
MyProg <flaviofds77@yahoo.com.br>
2024-12-01 18:17:47 -0300

    Update CSVSEI901.py

--- CSVSEI901.py
+++ CSVSEI901.py
@@ -74,6 +74,13 @@ def buscar_peso_no_packinglist(produto, packinglist_df):
 
 # Configura o locale para o padrão brasileiro
 #locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+try:
+    # Tenta definir o locale para pt_BR.UTF-8
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+except locale.Error:
+    # Se não for possível, usa um fallback (como 'en_US.UTF-8')
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+    print("Locale 'pt_BR.UTF-8' não disponível, utilizando 'en_US.UTF-8' como fallback.")
 
 
 def formatar_valor(valor):
