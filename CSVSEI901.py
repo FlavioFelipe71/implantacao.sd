c0a9036d4cbda97314fee4d5e335fd67986b2aec
MyProg <flaviofds77@yahoo.com.br>
2024-12-01 23:34:15 -0300

    Update CSVSEI901.py

--- CSVSEI901.py
+++ CSVSEI901.py
@@ -8,11 +8,11 @@ import base64
 # Configurar a largura da página para "wide" ao fechar a sidebar
 st.set_page_config(page_title="CSVSEI901", layout="wide", page_icon="🌲")
 
-    hide_footer_style = """
-    <style>
-    .reportview-container .main footer {visibility: hidden;}    
-    """
-    st.markdown(hide_footer_style, unsafe_allow_html=True)
+hide_footer_style = """
+<style>
+.reportview-container .main footer {visibility: hidden;}    
+"""
+st.markdown(hide_footer_style, unsafe_allow_html=True)
 ###########################################################################
 # CSS personalizado para ocultar o rodapé original e definir um novo rodapé
 footer = """
